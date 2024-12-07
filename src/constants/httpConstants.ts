c5e44ea30b4fb3cfd428049918cdb1523a9087ae
Matt D <mattwdavies@hotmail.co.uk>
2024-12-07 13:12:23 +0000

    feat: remove pre-commit and husky (for now)

--- src/constants/httpConstants.ts
+++ src/constants/httpConstants.ts
@@ -4,7 +4,7 @@ export enum HttpStatus {
   NO_CONTENT = 204,
   BAD_REQUEST = 400,
   NOT_FOUND = 404,
-  INTERNAL_SERVER_ERROR = 500,
+  INTERNAL_SERVER_ERROR = 500
 }
 
 export enum ErrorMessage {
